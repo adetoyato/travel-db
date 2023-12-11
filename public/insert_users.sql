09c3e5e0bccb1cea5717a2fddc96778af682abf9
travel <alexis.detoyato@newbrighton.edu.ph>
2023-12-11 10:28:32 +0800

    -edited insert procedure for users table

--- public/insert_users.sql
+++ public/insert_users.sql
@@ -1,4 +1,4 @@
-create procedure insert_users(IN p_user_id integer, IN p_fname character, IN p_lname character, IN p_age integer, IN p_username character varying, IN p_password character varying, IN p_role_id integer, IN p_role character varying)
+create procedure insert_users(IN p_user_id integer, IN p_fname character, IN p_lname character, IN p_age integer, IN p_username character varying, IN p_password character varying, IN p_role_id integer)
     language plpgsql
 as
 $$
@@ -31,14 +31,10 @@ BEGIN
         RAISE EXCEPTION 'role_id must have a value.';
     end if;
 
-    if p_role is NULL then
-        RAISE EXCEPTION 'role must be defined.';
-    end if;
-
-    insert into users(user_id, fName, lName, age, username, password, role_id, role)
-    values(p_user_id, p_fName, p_lName, p_age, p_username, p_password, p_role_id, p_role);
+    insert into users(user_id, fName, lName, age, username, password, role_id)
+    values(p_user_id, p_fName, p_lName, p_age, p_username, p_password, p_role_id);
 END;
 $$;
 
-alter procedure insert_users(integer, char, char, integer, varchar, varchar, integer, varchar) owner to postgres;
+alter procedure insert_users(integer, char, char, integer, varchar, varchar, integer) owner to postgres;
 
